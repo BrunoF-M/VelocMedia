ab5664cd9d063c88e675746e085a4ae423a0a4a5
BrunoF-M <bruno.freitas.marmol77.bm@gmail.com>
2020-10-07 06:51:34 -0300

    Resolução do Exercício

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace VelocMedia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            int distancia;
+            int tempo;
+            
+            Console.Write("Distancia percorrida (m): ");
+            distancia = int.Parse(Console.ReadLine());
+
+            Console.Write("Tempo gasto (s): ");
+            tempo = int.Parse(Console.ReadLine());
+
+            int velocidade;
+
+            velocidade = (distancia/tempo);
+
+            Console.WriteLine();
+
+            Console.WriteLine($"Velocidade média: {velocidade} m/s");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Pressione qualquer tecla para finalizar...");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
